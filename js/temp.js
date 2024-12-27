f4ea33626fcb93620eaa45297cbd645f3a616064
takoong <takoong@atoncorp.com>
2024-12-27 15:47:23 +0900

    깃플챗 라이프 플래닛 샘플 작업중.

--- js/temp.js
+++ js/temp.js
@@ -1,32 +1,48 @@
-
+/* local test */
 
 
 /* 왼쪽 메뉴 테스트 */
-let asideMenus = document.querySelectorAll(".al-sidebar-list > li a");
+let alSidebarList = document.querySelector(".al-sidebar-list");
+let alSidebarSublist =  alSidebarList.querySelectorAll(".al-sidebar-sublist");
+let asideMenus = document.querySelectorAll(".al-sidebar-list > li a.aside-list-link");
 asideMenus.forEach(function(itemA){
     itemA.addEventListener("click", () => {
         
         let li = itemA.closest("li");
+        let liSubList = li.querySelector(".al-sidebar-sublist");
 
-        if(li.classList.contains("active")) {
-            asideMenus.forEach(function(itemLi){
-                itemLi.closest("li").classList.remove("active");
-                document.querySelector(".container_wrap .page_wrap").style.paddingLeft = "";
+        if(li.classList.contains("active")) { // active 활성화 확인
+
+            if(liSubList && liSubList.classList.contains("open")) { // 서브메뉴가 있고 오픈되어있는 경우 닫기
+                liSubList.classList.remove("open");
+                closeAside();
+            } else if(liSubList && !liSubList.classList.contains("open")) { // 서브메뉴가 있고 오픈되어있지 않은 경우 오픈
+                liSubList.classList.add("open");
+                openAside();
+            }
+        } else { // active가 활성화 되지 않은 메뉴 클릭시
+            alSidebarSublist.forEach(function(alSidebarSublist_item){
+                alSidebarSublist_item.classList.remove("open");
             })
-        } else {
             asideMenus.forEach(function(itemLi){
-                itemLi.closest("li").classList.remove("active");
+                itemLi.closest("li").classList.remove("active"); // 기존에 열려있는 메뉴 전부 닫기
             })
-            li.classList.add("active");
-            document.querySelector(".container_wrap .page_wrap").style.paddingLeft = "calc(200px + 1em)";
+            closeAside();
+
+            li.classList.add("active"); // 클릭된 메뉴 활성화
+            if(liSubList) { // 활성화된 메뉴에 서브메뉴가 있으면 
+                liSubList.classList.add("open");
+                openAside();
+            }
         }
-
-        
-        // asideMenus.closest("li").classList.remove("active")
-        
-        
-        
-        // item.closest("li").classList.add("active");
     })
 })
+
+function closeAside(){
+    document.querySelector(".container_wrap .page_wrap").style.paddingLeft = "";
+}
+
+function openAside(){
+    document.querySelector(".container_wrap .page_wrap").style.paddingLeft = "calc(200px + 1em)";
+}
 /* // 왼쪽 메뉴 테스트 */
